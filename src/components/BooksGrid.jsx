40f21e6e2fad0985abf8a132bb7f05a895d9d100
Lokesh <lokeshdebnath8@gmail.com>
2024-01-18 17:57:32 +0530

    after adding data

--- src/components/BooksGrid.jsx
+++ src/components/BooksGrid.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
+import { bookLists } from "./../utils/data";
 import Book from "./Book";
 
 export default function BooksGrid() {
+  const initialState = bookLists;
+
+  const [books, setBooks] = useState(initialState);
+  console.log(books);
   return (
     <div className="container mx-auto grid grid-cols-1 gap-8 max-w-7xl md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       <Book />
